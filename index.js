e71137497dcf24b8545940af49c01d35332de6b8
blankspacey <rayan.capuzzi@gmail.com>
2022-02-19 19:21:03 +0100

    changed some stuff

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const Discord = require('discord.js');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const prefix = 'eelon';
 
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES], partials: ['CHANNEL'] });
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.command.set(command.name, command);
+}
+dotenv.config();
+
+const client = new Discord.Client({
+	intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGES],
+	partials: ['CHANNEL']
+});
 
 client.on('ready', function() {
 	console.log('bot is ready');
 });
 
 client.on('messageCreate', function(message) {
-	if (message.author.bot) return;
-	message.reply('eelon musck is listening');
-	message.author.send('eelon musck says hi');
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+	const arg = message.content.slice(prefix.length).split(/\s+/);
+	const command = arg.shift().toLowerCase();
+
+	if (command === 'eelon') {
+		message.channel.send('');
+	}
 });
 
-client.login('OTI3NjAzMjgxMzczNTY5MDU0.YdMn4A.b_gOnIfWB8JGXkeHg4YvQSqnSeE');
+client.login(process.env.TOKEN);
