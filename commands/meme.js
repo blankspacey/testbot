748c530b8249816ed62318fce2f993c1eba8240b
Danebi <57720273+danebidev@users.noreply.github.com>
2022-02-27 00:16:48 +0100

    Meme command now uses async/await. Added check for post_hint different from 'image'

--- commands/meme.js
+++ commands/meme.js
@@ -2,16 +2,22 @@ module.exports = {
 	name: 'meme',
 	minArg: 0,
 	maxArgs: 0,
-	execute: function(message, args, client) {
+	execute: async function(message, args, client) {
+
 		const subreddit = client.snoowrap.getSubreddit('memesITA');
-		subreddit.getRandomSubmission({ time: 'week' }).fetch().then(subInfo => {
-			const messageEmbed = {
-				color: 0x0099ff,
-				title: subInfo.title,
-				image: { url: subInfo.url }
-			};
-			message.reply({ embeds: [messageEmbed] });
-		});
+		let submission = await subreddit.getRandomSubmission({ time: 'week' }).fetch();
+
+		while(submission.post_hint != 'image') submission = await subreddit.getRandomSubmission({ time: 'week' }).fetch();
+
+		const messageEmbed = {
+
+			color: 0x0099ff,
+			title: submission.title,
+			image: { url: submission.url }
+
+		};
+
+		message.reply({ embeds: [messageEmbed] });
 
 	}
 };
